798422c021a699ff58cf2c6a7f3e3c69e2ed37f7
EC2 Default User <ec2-user@ip-172-31-3-178.ap-northeast-1.compute.internal>
2022-05-29 13:40:44 +0000

    [ADD]Genresテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_29_132742) do
+ActiveRecord::Schema.define(version: 2022_05_29_133819) do
+
+  create_table "genres", force: :cascade do |t|
+    t.integer "item_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.integer "genre_id"
