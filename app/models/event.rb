135238b0321aa038ef5d2fe71b4ff871fea2a62c
EC2 Default User <ec2-user@ip-172-31-3-178.ap-northeast-1.compute.internal>
2022-05-30 12:26:36 +0000

    [ADD]eventコントローラー編集

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,17 @@
 class Event < ApplicationRecord
+  
+  def index
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  private
+  
+  def event_params
+    params.require(:event).parmit(:title, :start_time, :user_id)
+  end
 end
