b2fdea70f461cc5bbe47f2cf53c7f16d714162a1
EC2 Default User <ec2-user@ip-172-31-3-178.ap-northeast-1.compute.internal>
2022-05-31 17:34:31 +0000

    [ADD]genresコントローラーのアクション挙動追加

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,29 +1,31 @@
 class GenresController < ApplicationController
   
   def new
-  
+    @genre = Genre.new
   end
   
   def create
-  
-  
-  
+    @genre = Genre.new(genre_params)
+    @genre.save
   end
   
   def index
-  
+    @genre = Gnere.all
   end
   
   def edit
-  
-  
-  
+    @genre = Genre.find(params[:id])
   end
   
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params
+)
+  end
   
   private
   
-  def genres_params
+  def genre_params
     params.require(:genre).permit(:item_id,:name)
   end
 end
