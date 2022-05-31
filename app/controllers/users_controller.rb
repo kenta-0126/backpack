f9f3aeef9ebbd0ea2e8b2f1d46c638ed5ca6fadf
EC2 Default User <ec2-user@ip-172-31-3-178.ap-northeast-1.compute.internal>
2022-05-31 16:53:27 +0000

    [ADD]userコントローラーのアクション挙動追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,28 @@
 class UsersController < ApplicationController
 
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
   end
 
   def update
-
+    @user = current_user
+    @user.update(user_params)
+    redirect_to user_my_page_path(@user)
   end
 
   def confirm
-
+    @user = current_user
   end
 
   def withdraw
+    @user = User.find(current_user.id)
+    @user.update(is_active: 1) #1で退会済みの論理削除
+    reset.session
+    redirect_to root_path
   end
 
   private
