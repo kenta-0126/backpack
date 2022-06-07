af7f7c9b5c1c76ffac2ce0a6d1a191fa63779746
EC2 Default User <ec2-user@ip-172-31-3-178.ap-northeast-1.compute.internal>
2022-06-07 16:35:27 +0000

    [ADD]userの登録情報の編集ページの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @events = Event.all
-    
+
   end
 
   def edit
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def update
     @user = current_user
     @user.update(user_params)
-    redirect_to user_my_page_path(@user)
+    redirect_to users_my_page_path(@user)
   end
 
   def confirm
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-   params.require(:user).parmit(:first_name, :last_name, :first_name_kana, :last_name_kana, :email, :is_active)
+   params.require(:user).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :email, :is_active)
   end
 
 end
