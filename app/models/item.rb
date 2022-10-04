93fe3863006023a2485b17dce4b003d8bec44146
EC2 Default User <ec2-user@ip-172-31-3-178.ap-northeast-1.compute.internal>
2022-10-04 08:05:41 +0000

    [ADD]itemにバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,10 @@
 class Item < ApplicationRecord
+  validates :genre_id, presence: true
+  validates :name, presence: true
+  
   belongs_to :genre
   belongs_to :user, optional: true
   has_many :event
 
-  validates :genre_id, presence: true
+  
 end
